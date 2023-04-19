34c4110de9cc506e1cecd15a6ab4c53ad79ad88d
White-Patrasche <5338095@gmail.com>
2023-04-19 13:42:06 +0900

    feat: set reducer functions in View

--- Task1/Task1/ContentView.swift
+++ Task1/Task1/ContentView.swift
@@ -15,6 +15,53 @@ struct ContentView: View {
     
     
     init(environment: CalculatorEnvironment) {
+        calculatorReducer = AnyReducer<CalculatorState, CalculatorAction, CalculatorEnvironment> {state, action, environment in switch action {
+        case let .firstNumberChanged(number):
+            state.firstNumber = number
+            return .none
+            
+        case let .secondNumberChanged(number):
+            state.secondNumber = number
+            return .none
+            
+        case .add:
+            if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
+                let result = environment.calculatorService.add(first, second)
+                state.resultString = "\(first) + \(second) = \(result)"
+            } else {
+                state.resultString = "잘못된 입력입니다"
+            }
+            return .none
+        case .sub:
+            if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
+                let result = environment.calculatorService.subtract(first, second)
+                state.resultString = "\(first) - \(second) = \(result)"
+            } else {
+                state.resultString = "잘못된 입력입니다"
+            }
+            return .none
+        case .multi:
+            if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
+                let result = environment.calculatorService.multiply(first, second)
+                state.resultString = "\(first) * \(second) = \(result)"
+            } else {
+                state.resultString = "잘못된 입력입니다"
+            }
+            return .none
+        case .div:
+            if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
+                if second == 0 {
+                    state.resultString = "0으로 나눌 수 없습니다!"
+                } else {
+                    let result = environment.calculatorService.divide(first, second)
+                    state.resultString = "\(first) / \(second) = \(result)"
+                }
+                
+            } else {
+                state.resultString = "잘못된 입력입니다"
+            }
+            return .none
+        }}
         
         self.store = Store(initialState: CalculatorState(), reducer: calculatorReducer, environment: environment)
         
@@ -66,53 +113,7 @@ struct CalculatorServiceImpl: CalculatorService {
     }
 }
 
-let calculatorReducer = AnyReducer<CalculatorState, CalculatorAction, CalculatorEnvironment> {state, action, environment in switch action {
-case let .firstNumberChanged(number):
-    state.firstNumber = number
-    return .none
-    
-case let .secondNumberChanged(number):
-    state.secondNumber = number
-    return .none
-    
-case .add:
-    if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
-        let result = environment.calculatorService.add(first, second)
-        state.resultString = "\(first) + \(second) = \(result)"
-    } else {
-        state.resultString = "잘못된 입력입니다"
-    }
-    return .none
-case .sub:
-    if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
-        let result = environment.calculatorService.subtract(first, second)
-        state.resultString = "\(first) - \(second) = \(result)"
-    } else {
-        state.resultString = "잘못된 입력입니다"
-    }
-    return .none
-case .multi:
-    if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
-        let result = environment.calculatorService.multiply(first, second)
-        state.resultString = "\(first) * \(second) = \(result)"
-    } else {
-        state.resultString = "잘못된 입력입니다"
-    }
-    return .none
-case .div:
-    if let first = Int(state.firstNumber), let second = Int(state.secondNumber) {
-        if second == 0 {
-            state.resultString = "0으로 나눌 수 없습니다!"
-        } else {
-            let result = environment.calculatorService.divide(first, second)
-            state.resultString = "\(first) / \(second) = \(result)"
-        }
-        
-    } else {
-        state.resultString = "잘못된 입력입니다"
-    }
-    return .none
-}}
+
 
 
 struct CalculatorView: View {
